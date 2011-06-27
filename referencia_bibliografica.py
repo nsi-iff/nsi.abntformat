a9978eacc73f6d677514abd2774d2a629caa47e9
ruhanfa <ruhan.fa@gmail.com>
2011-06-27 15:20:48 -0300

    first commit

---
+++ referencia_bibliografica.py
@@ -0,0 +1,114 @@
+#coding:utf-8
+
+class ReferenciaBibliografica:
+    def __init__(self):
+        pass
+
+    def _monta_nome(self, autores):
+        lista_autores = autores.split(';')
+        for i in xrange(len(lista_autores)):
+            nome_autor = lista_autores[i].split(' ')
+            if nome_autor.__contains__(''):
+                nome_autor.remove('')
+            nome_abnt = nome_autor.pop(len(nome_autor) - 1).upper() + ','
+            for j in xrange(len(nome_autor)):
+                nome_abnt += ' ' + nome_autor[j][:1:] + '.'
+            lista_autores[i] = nome_abnt
+        return "; ".join(lista_autores)
+
+    def _referencia_trabalho_conclusao(self, trabalho_conclusao):
+        referencia = self._monta_nome(trabalho_conclusao.autores)
+        referencia += ' ' + trabalho_conclusao.titulo
+        if trabalho_conclusao.subtitulo != None:
+            referencia += ': '
+            referencia += trabalho_conclusao.subtitulo
+        referencia += '. '
+        referencia += trabalho_conclusao.data_defesa + '. '
+        referencia += trabalho_conclusao.total_folhas + ' f. '
+        referencia += trabalho_conclusao.tipo_trabalho + ' - '
+        referencia += trabalho_conclusao.instituicao + ', '
+        referencia += trabalho_conclusao.local_defesa + '.'
+        return referencia
+
+    def _referencia_artigo_anais_evento(self, artigo_anais_evento):
+        referencia = self._monta_nome(artigo_anais_evento.autores)
+        referencia += ' ' + artigo_anais_evento.titulo
+        if artigo_anais_evento.subtitulo != None:
+            referencia += ': '
+            referencia += artigo_anais_evento.subtitulo
+        referencia += '. In: '
+        referencia += artigo_anais_evento.nome_evento + ', '
+        referencia += artigo_anais_evento.numero_evento + '., '
+        referencia += artigo_anais_evento.ano_evento + ', '
+        referencia += artigo_anais_evento.local_evento + '. '
+        referencia += artigo_anais_evento.titulo_anais + '. '
+        referencia += artigo_anais_evento.local_publicacao + ': '
+        referencia += artigo_anais_evento.editora + ', '
+        referencia += artigo_anais_evento.ano_publicacao + '. '
+        referencia += 'P. ' + artigo_anais_evento.pagina_inicial + '-' + artigo_anais_evento.pagina_final + '.'
+        return referencia
+
+    def _referencia_artigo_periodico(self, artigo_periodico):
+        referencia = self._monta_nome(artigo_periodico.autores)
+        referencia += ' ' + artigo_periodico.titulo
+        if artigo_periodico.subtitulo != None:
+            referencia += ': '
+            referencia += artigo_periodico.subtitulo
+        referencia += '. '
+        referencia += artigo_periodico.nome_periodico + ', '
+        referencia += artigo_periodico.local_publicacao + ', '
+        referencia += 'v. ' + artigo_periodico.volume + ', '
+        referencia += 'n. ' + artigo_periodico.fasciculo + ', '
+        referencia += 'p. ' + artigo_periodico.pagina_inicial + '-' + artigo_periodico.pagina_final + ', '
+        referencia += artigo_periodico.data_publicacao + '.'
+        return referencia
+
+    def _referencia_periodico_tecnico_cientifico(self, periodico_tecnico_cientifico):
+        referencia = periodico_tecnico_cientifico.titulo + '. '
+        referencia += periodico_tecnico_cientifico.local_publicacao + ': '
+        referencia += periodico_tecnico_cientifico.editora + ', '
+        referencia += periodico_tecnico_cientifico.ano_primeiro_volume + '-'
+        if periodico_tecnico_cientifico.ano_ultimo_volume != None:
+            referencia += periodico_tecnico_cientifico.ano_ultimo_volume + '.'
+        return referencia
+
+    def _referencia_livro(self, livro):
+        referencia = self._monta_nome(livro.autores)
+        referencia += ' ' + livro.titulo
+        if livro.subtitulo != None:
+            referencia += ': '
+            referencia += livro.subtitulo
+        referencia += '. '
+        if livro.traducao != None:
+            referencia += livro.traducao + '. '
+        if livro.edicao != None:
+            referencia += livro.edicao + '. '
+        referencia += livro.local_publicacao + ': '
+        referencia += livro.editora + ', '
+        referencia += livro.ano_publicacao + '. '
+        referencia += livro.numero_paginas + ' p.'
+        return referencia
+
+    def _referencia_ralatorio_tecnico_cientifico(self, relatorio_tecnico_cientifico):
+        referencia = self._monta_nome(relatorio_tecnico_cientifico.autores)
+        referencia += ' ' + relatorio_tecnico_cientifico.titulo + '. '
+        referencia += relatorio_tecnico_cientifico.local_publicacao + ': '
+        referencia += relatorio_tecnico_cientifico.instituicao + ', '
+        referencia += relatorio_tecnico_cientifico.ano_publicacao + '. '
+        referencia += relatorio_tecnico_cientifico.numero_paginas + ' p.'
+        return referencia
+
+    def gera_referencia(self, documento):
+        if documento.tipo == 'trabalho de conclusão':
+            return self._referencia_trabalho_conclusao(documento)
+        elif documento.tipo == 'artigo de anais de eventos':
+            return self._referencia_artigo_anais_evento(documento)
+        elif documento.tipo == 'artigo de periodico':
+            return self._referencia_artigo_periodico(documento)
+        elif documento.tipo == 'periodico tecnico cientifico':
+            return self._referencia_periodico_tecnico_cientifico(documento)
+        elif documento.tipo == 'livro':
+            return self._referencia_livro(documento)
+        elif documento.tipo == 'relatorio tecnico cientifico':
+            return self._referencia_ralatorio_tecnico_cientifico(documento)
+
---
+++ referencia_bibliografica_spec.py
@@ -0,0 +1,111 @@
+#coding:utf-8
+
+from ludibrio import Stub
+import unittest
+from should_dsl import should
+from referencia_bibliografica import ReferenciaBibliografica
+
+
+class ReferenciaBibliograficaSpec(unittest.TestCase):
+
+    def teste_monta_nome(self):
+        montar_nome = ReferenciaBibliografica()
+        montar_nome._monta_nome('Ruhan Ferreira Almeida; Carlos Souza Teste') |should| equal_to ('ALMEIDA, R. F.; TESTE, C. S.')
+
+    def testa_referencia_de_trabalho_conclusao(self):
+        referencia = ReferenciaBibliografica()
+        with Stub() as trabalho_conclusao:
+            trabalho_conclusao.tipo >> 'trabalho de conclusão'
+            trabalho_conclusao.autores >> 'Ian Fantucci'
+            trabalho_conclusao.titulo >> 'Contribuição do alerta, da atenção, da intenção e da expectativa temporal para o desempenho de humanos em tarefas de tempo de reação'
+            trabalho_conclusao.subtitulo >> None
+            trabalho_conclusao.data_defesa >> '2001'
+            trabalho_conclusao.total_folhas >> '130'
+            trabalho_conclusao.tipo_trabalho >> 'Tese (Doutorado em Psicologia)'
+            trabalho_conclusao.instituicao >> 'Instituto de Psicologia, Universidade de São Paulo'
+            trabalho_conclusao.local_defesa >> 'São Paulo'
+        referencia_abnt = referencia.gera_referencia(trabalho_conclusao)
+        referencia_abnt |should| equal_to('FANTUCCI, I. Contribuição do alerta, da atenção, da intenção e da expectativa temporal para o desempenho de humanos em tarefas de tempo de reação. 2001. 130 f. Tese (Doutorado em Psicologia) - Instituto de Psicologia, Universidade de São Paulo, São Paulo.')
+
+    def testa_referencia_de_artigo_anais_evento(self):
+        referencia = ReferenciaBibliografica()
+        with Stub() as artigo_anais_evento:
+            artigo_anais_evento.tipo >> 'artigo de anais de eventos'
+            artigo_anais_evento.autores >> 'Antônio Fernandes Bueno Moreira'
+            artigo_anais_evento.titulo >> 'Multiculturalismo, Currículo e Formação de Professores'
+            artigo_anais_evento.subtitulo >> None
+            artigo_anais_evento.nome_evento >> 'SEMINÁRIO ESTADUAL DE EDUCAÇÃO BÁSICA'
+            artigo_anais_evento.numero_evento >> '2'
+            artigo_anais_evento.ano_evento >> '1998'
+            artigo_anais_evento.local_evento >> 'Santa Cruz do Sul'
+            artigo_anais_evento.titulo_anais >> 'Anais'
+            artigo_anais_evento.local_publicacao >> 'Santa Cruz do Sul'
+            artigo_anais_evento.editora >> 'EDUNISC'
+            artigo_anais_evento.ano_publicacao >> '1998'
+            artigo_anais_evento.pagina_inicial >> '15'
+            artigo_anais_evento.pagina_final >> '30'
+        referencia_abnt = referencia.gera_referencia(artigo_anais_evento)
+        referencia_abnt |should| equal_to('MOREIRA, A. F. B. Multiculturalismo, Currículo e Formação de Professores. In: SEMINÁRIO ESTADUAL DE EDUCAÇÃO BÁSICA, 2., 1998, Santa Cruz do Sul. Anais. Santa Cruz do Sul: EDUNISC, 1998. P. 15-30.')
+
+    def testa_referencia_de_artigo_periodico(self):
+        referencia = ReferenciaBibliografica()
+        with Stub() as artigo_periodico:
+            artigo_periodico.tipo >> 'artigo de periodico'
+            artigo_periodico.autores >> 'Demerval Saviani'
+            artigo_periodico.titulo >> 'A Universidade e a Problemática da Educação e Cultura'
+            artigo_periodico.subtitulo >> None
+            artigo_periodico.nome_periodico >> 'Educação Brasileira'
+            artigo_periodico.local_publicacao >> 'Brasília'
+            artigo_periodico.volume >> '1'
+            artigo_periodico.fasciculo >> '3'
+            artigo_periodico.pagina_inicial >> '35'
+            artigo_periodico.pagina_final >> '58'
+            artigo_periodico.data_publicacao >> '1979'
+        referencia_abnt = referencia.gera_referencia(artigo_periodico)
+        referencia_abnt |should| equal_to('SAVIANI, D. A Universidade e a Problemática da Educação e Cultura. Educação Brasileira, Brasília, v. 1, n. 3, p. 35-58, 1979.')
+
+    def testa_referencia_de_periodico_tecnico_cientifico(self):
+        referencia = ReferenciaBibliografica()
+        with Stub() as periodico_tecnico_cientifico:
+            periodico_tecnico_cientifico.tipo >> 'periodico tecnico cientifico'
+            periodico_tecnico_cientifico.titulo >> 'EDUCAÇÃO & REALIDADE'
+            periodico_tecnico_cientifico.local_publicacao >> 'Porto Alegre'
+            periodico_tecnico_cientifico.editora >> 'UFRGS/FACED'
+            periodico_tecnico_cientifico.ano_primeiro_volume >> '1975'
+            periodico_tecnico_cientifico.ano_ultimo_volume >> None
+        referencia_abnt = referencia.gera_referencia(periodico_tecnico_cientifico)
+        referencia_abnt |should| equal_to('EDUCAÇÃO & REALIDADE. Porto Alegre: UFRGS/FACED, 1975-')
+
+    def testa_referencia_de_livro(self):
+        referencia = ReferenciaBibliografica()
+        with Stub() as livro:
+            livro.tipo >> 'livro'
+            livro.autores >> 'Marcos Antônio Azevedo; Vinícios Nogueira Almeida Guerra'
+            livro.titulo >> 'Mania de bater'
+            livro.subtitulo >> 'a punição corporal doméstica de crianças e adolescentes no Brasil'
+            livro.traducao >> None
+            livro.edicao >> None
+            livro.local_publicacao >> 'São Paulo'
+            livro.editora >> 'Iglu'
+            livro.ano_publicacao >> '2001'
+            livro.numero_paginas >> '386'
+        referencia_abnt = referencia.gera_referencia(livro)
+        referencia_abnt |should| equal_to('AZEVEDO, M. A.; GUERRA, V. N. A. Mania de bater: a punição corporal doméstica de crianças e adolescentes no Brasil. São Paulo: Iglu, 2001. 386 p.')
+
+    def testa_referencia_de_relatorio_tecnico_cientifico(self):
+        referencia = ReferenciaBibliografica()
+        with Stub() as relatorio_tecnico_cientifico:
+            relatorio_tecnico_cientifico.tipo >> 'relatorio tecnico cientifico'
+            relatorio_tecnico_cientifico.autores >> 'Ubiraci Espinelli Lemes Souza; Silvio Burratino Melhado'
+            relatorio_tecnico_cientifico.titulo >> 'Subsídios para a avaliação do custo de mão-de-obra na construção civil'
+            relatorio_tecnico_cientifico.local_publicacao >> 'São Paulo'
+            relatorio_tecnico_cientifico.instituicao >> 'EPUSP'
+            relatorio_tecnico_cientifico.ano_publicacao >> '1991'
+            relatorio_tecnico_cientifico.numero_paginas >> '38'
+        referencia_abnt = referencia.gera_referencia(relatorio_tecnico_cientifico)
+        referencia_abnt |should| equal_to('SOUZA, U. E. L.; MELHADO, S. B. Subsídios para a avaliação do custo de mão-de-obra na construção civil. São Paulo: EPUSP, 1991. 38 p.')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
